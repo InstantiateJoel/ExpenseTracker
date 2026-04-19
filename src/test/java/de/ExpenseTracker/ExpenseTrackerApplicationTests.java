f3f53e68ad8573b815d2d6d5884bff746a377140
joel <joel.scholz.vib@gmail.com>
2026-04-19 21:32:34 +0200

    - added missing schema to application.properties - added mising active profiles to ExpenseTrackerApplicationTests.java

--- src/test/java/de/ExpenseTracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/de/ExpenseTracker/ExpenseTrackerApplicationTests.java
@@ -2,8 +2,10 @@ package de.ExpenseTracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExpenseTrackerApplicationTests {
 
 	@Test
